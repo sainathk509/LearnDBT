4158bef6902c425e99b3ca1c1ac989674cd09e18
Sainath Krishnaswamy <Sainathk509@gmail.com>
2024-06-28 20:33:03 +0000

    commit

--- models/dim_customers.sql
+++ models/dim_customers.sql
@@ -1,55 +1,59 @@
-with customers as (
+{{ config(materialized="table") }}
 
-    select
-        id as customer_id,
+
+with
+    customers as (
+
+        select *
+        /*id as customer_id,
         first_name,
-        last_name
+        last_name */
+        -- from raw.jaffle_shop.customers
+        from {{ ref("stage_jaffle_shop__customer") }}
 
-    from raw.jaffle_shop.customers
+    ),
 
-),
+    orders as (
 
-orders as (
-
-    select
-        id as order_id,
+        select *
+        /*id as order_id,
         user_id as customer_id,
         order_date,
-        status
+        status */
+        -- from raw.jaffle_shop.orders
+        from {{ ref("stage_jaffle_shop__orders") }}
+    ),
 
-    from raw.jaffle_shop.orders
+    customer_orders as (
 
-),
+        select
+            customer_id,
 
-customer_orders as (
+            min(order_date) as first_order_date,
+            max(order_date) as most_recent_order_date,
+            count(order_id) as number_of_orders
 
-    select
-        customer_id,
+        from orders
 
-        min(order_date) as first_order_date,
-        max(order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders
+        group by 1
 
-    from orders
+    ),
 
-    group by 1
+    final as (
 
-),
+        select
+            customers.customer_id,
+            customers.first_name,
+            customers.last_name,
+            customer_orders.first_order_date,
+            customer_orders.most_recent_order_date,
+            coalesce(customer_orders.number_of_orders, 0) as number_of_orders
 
-final as (
+        from customers
 
-    select
-        customers.customer_id,
-        customers.first_name,
-        customers.last_name,
-        customer_orders.first_order_date,
-        customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
+        left join customer_orders using (customer_id)
 
-    from customers
+    )
 
-    left join customer_orders using (customer_id)
-
-)
-
-select * from final
+select *
+from final
